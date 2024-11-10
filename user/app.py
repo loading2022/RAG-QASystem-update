fee2cde1d95db68f3d0caba5ac6a8df27bb2493c
ShihPing0803 <119124535+loading2022@users.noreply.github.com>
2024-11-10 22:28:55 +0800

    Using whisper with Azure OpenAI

--- user/app.py
+++ user/app.py
@@ -5,44 +5,29 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain_community.callbacks import get_openai_callback
 from langchain_openai import AzureChatOpenAI
 from opencc import OpenCC
-from openai import OpenAI
+from openai import AzureOpenAI
 from dotenv import load_dotenv
-from pydub import AudioSegment
-import tempfile
 import io
 import os
-import whisper
-import openai
-from langchain_openai import ChatOpenAI
-from langchain_openai import OpenAIEmbeddings
-from langchain.retrievers.multi_query import MultiQueryRetriever
+
 
 load_dotenv()
-client = OpenAI()
-model = whisper.load_model("base")
-openai.api_key = os.getenv('OPENAI_API_KEY')
-embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
 
-#os.environ["AZURE_OPENAI_ENDPOINT"] = os.getenv("AZURE_OPENAI_ENDPOINT")
-#os.environ["AZURE_OPENAI_API_KEY"] = os.getenv("AZURE_OPENAI_API_KEY")
+os.environ["AZURE_OPENAI_ENDPOINT"] = os.getenv("AZURE_OPENAI_ENDPOINT")
+os.environ["AZURE_OPENAI_API_KEY"] = os.getenv("AZURE_OPENAI_API_KEY")
 
-"""embeddings = AzureOpenAIEmbeddings(
+embeddings = AzureOpenAIEmbeddings(
         model="text-embedding-ada-002",
         openai_api_version=os.getenv("AZURE_OPENAI_EMBEDDING_API_VERSION")
-)"""
+)
 app = Flask(__name__)
-"""vector_store = Chroma(
+vector_store = Chroma(
     collection_name = "fareastonHR",
     embedding_function = AzureOpenAIEmbeddings(
         model="text-embedding-ada-002",
         openai_api_version=os.getenv("AZURE_OPENAI_EMBEDDING_API_VERSION")
     ),
     persist_directory = "../db/fareastonHRupdate2"
-)"""
-vector_store = Chroma(
-    #collection_name="fareastonHRupdate1",
-    embedding_function=OpenAIEmbeddings(),
-    persist_directory="../db/fareastonHR"
 )
 
 class NamedBytesIO(io.BytesIO):
@@ -62,34 +47,17 @@ def index():
 @app.route('/get_response', methods=['POST'])
 def get_response():
     user_input = request.form.get('user_input')
+    db = None
     if not user_input:
         return jsonify({'error': 'No user input provided'})
     if user_input:
-        sources = []
-        llm = ChatOpenAI(temperature=0)
-        retriever_from_llm = MultiQueryRetriever.from_llm(
-            retriever=vector_store.as_retriever(), llm=llm
-        )
-        
-        docs = retriever_from_llm.invoke(user_input)
-        
-        #docs = vector_store.similarity_search_with_relevance_scores(user_input)
-    
-        for doc in docs:
-            doc_source = doc.metadata['source']
-            if doc_source in sources:
-                continue
-            sources.append(doc_source)
-            
-        """llm = AzureChatOpenAI(
+        docs = vector_store.similarity_search(user_input)
+        llm = AzureChatOpenAI(
             azure_deployment = os.getenv("AZURE_OPENAI_DEPLOYMENT"),
             api_version = os.getenv("AZURE_OPENAI_CHAT_API_VERSION"),
             temperature = 0.2
-        )"""
-        llm = ChatOpenAI(
-            model_name="gpt-4o",
-            temperature=0.5
         )
+
         chain = load_qa_chain(llm, chain_type="stuff")
 
         with get_openai_callback() as cb:
@@ -97,37 +65,32 @@ def get_response():
         cc = OpenCC('s2t')
         answer=cc.convert(response['output_text'])
         chat_history.append({'user': user_input, 'assistant': response['output_text']})
-        return jsonify({'response': answer, "sources": sources})
+        return jsonify({'response': answer})
 
 @app.route('/upload-audio', methods=['POST'])
 def upload_audio():
+    client = AzureOpenAI(
+        api_key=os.getenv("AZURE_WHISPER_API_KEY"),  
+        api_version= os.getenv("AZURE_WHISPER_API_VERSION"),
+        azure_endpoint = os.getenv("AZURE_WHISPER_ENDPOINT")
+    )
+    
+    whisper_deployment_id = os.getenv("AZURE_WHISPER_DEPLOYMENT") 
     if 'audio' not in request.files:
         return jsonify({'error': '沒有接收到音訊文件'}), 400
 
     audio_file = request.files['audio']
-    
     if audio_file:
-        audio_data = audio_file.read()
-        with tempfile.NamedTemporaryFile(delete=False, suffix='.webm') as temp_audio:
-            temp_audio.write(audio_data)
-            temp_audio_path = temp_audio.name
+        audio_stream = NamedBytesIO(audio_file.read())
+        audio_stream.name = 'transcript.wav' 
 
-        try:
-            audio = AudioSegment.from_file(temp_audio_path, format="webm")
-            wav_path = temp_audio_path.replace('.webm', '.wav')
-            audio.export(wav_path, format="wav")
-            result = model.transcribe(wav_path, fp16=False)
-            transcript = result["text"]
-            cc = OpenCC('s2t')
-            text = cc.convert(transcript)
-            return jsonify({'message': '音檔已處理', 'transcript': text})
-        except Exception as e:
-            return jsonify({'error': f'處理音訊時發生錯誤: {str(e)}'}), 500
-        finally:
-            os.unlink(temp_audio_path)
-            if os.path.exists(wav_path):
-                os.unlink(wav_path)
-
+        transcript = client.audio.transcriptions.create(
+            file=audio_stream,          
+            model=whisper_deployment_id
+        )
+        cc = OpenCC('s2t')
+        text = cc.convert(transcript.text)
+        return jsonify({'message': '音頻已處理', 'transcript': text})
     return jsonify({'error': '沒有接收到音訊文件'}), 400
 
 if __name__ == '__main__':
